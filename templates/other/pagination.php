2218e80265bc803d4c8ac3ff074d0ce6abb8c9fd
Alex Chapin <achapin@gmail.com>
2017-11-28 12:46:20 -0500

    add code for wp_pagenavi plugin use

--- templates/other/pagination.php
+++ templates/other/pagination.php
@@ -1,21 +1,19 @@
 <?php // vtt_print('default:other:pagination'); ?>
+<?php global $wp_query; ?>
 
+<?php if( $wp_query->max_num_pages > 1 ): ?>
 
-<?php
-global $wp_query;
-
-if( $wp_query->max_num_pages > 1 ):
-
-	?>
 	<div id="page-navigation" role="navigation">
+	<?php if( function_exists('wp_pagenavi') ) : ?>
+		<?php  wp_pagenavi(); ?>
+	<?php else : ?>
 		<div class="nav-next">
 			<?php next_posts_link( '&laquo; Older Items' ); ?>
 		</div>
 		<div class="nav-prev">
 			<?php previous_posts_link( 'Newer Items &raquo;' ); ?>
 		</div>
+	<?php endif; ?>
 	</div>
-	<?php
 
-endif;
-?>
+<?php endif; ?>
