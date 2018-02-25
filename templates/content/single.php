726ca4efc2bcddc6b3823d7fc71a89bd2c5f39df
Alex Chapin <achapin@gmail.com>
2018-02-25 13:44:36 -0500

    add support for displaying harwood gardens taxonomies and fields

--- templates/content/single.php
+++ templates/content/single.php
@@ -2,28 +2,34 @@
 <?php
 global $post;
 $featured_image_position = $vtt_config->get_value( 'featured-image-position' );
+
 if( $post->post_type === 'references') {
 // 	$year_terms = get_the_terms($post, 'year');
 // 	$year = $year_terms[0];
 	$year = labs_get_acf_select_value( 'publication_year' );
 }
+//printpre($_SESSION);
 ?>
 
 
 <div class="page-title">
 	<div class="breadcrumbs"><?php echo vtt_get_breadcrumbs( $post ); ?></div>
+	
 	<?php if( $post->post_type === 'references'): ?>
 		<?php echo '<h1>'.$year." - ".vtt_get_page_title().'</h1>'; ?>
+		
 	<?php elseif( $post->post_type === 'plants'): ?>
 		<?php echo '<h1>'.vtt_get_page_title().'</h1>'; ?>
-		<?php echo "<div id='common-name'>"; ?>
-		<?php echo the_terms( $post->ID, "common_names" ); ?>
-		<?php echo  " (Location: "; ?>
-		<?php echo the_terms( $post->ID, "locations" ); ?>
-		<?php echo " "; ?>
-		<?php echo the_terms( $post->ID, "directions" ); ?>
-		<?php echo ")"; ?>
-		<?php echo "</div>"; ?>	
+		<?php if (!is_object(get_taxonomy( 'genus')) ): ?>
+			<?php echo "<div id='common-name'>"; ?>
+			<?php echo the_terms( $post->ID, "common_names" ); ?>
+			<?php echo  " (Location: "; ?>
+			<?php echo the_terms( $post->ID, "locations" ); ?>
+			<?php echo " "; ?>
+			<?php echo the_terms( $post->ID, "directions" ); ?>
+			<?php echo ")"; ?>
+			<?php echo "</div>"; ?>	
+		<?php endif; ?>
 	<?php else:?>	
 		<?php echo '<h1>'.vtt_get_page_title().'</h1>'; ?>
 	<?php endif; ?>
@@ -135,9 +141,121 @@ if( $post->post_type === 'references') {
 		<span class="ref-label ref-question-label">Relevance:</span> 
 		<?php the_field('relevance'); ?>
 		</div>
-
-							
 		<?php echo labs_get_taxonomy_terms('archive', $post); ?>
+	<?php endif; ?>
+	
+	<?php if( $post->post_type === 'plants' && is_object(get_taxonomy( 'genus')) ): ?>
+		<?php echo "<div class='divTable plants-table'>"; ?>
+		<?php echo "<div class='divTableHeading'>"; ?>
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableHead'>Genus</div>"; ?>
+		<?php echo "<div class='divTableHead'>Species</div>"; ?>
+		<?php echo "<div class='divTableHead'>Common Name</div>"; ?>
+		<?php echo "</div>"; ?>
+		<?php echo "</div>"; ?>
+		<?php echo "<div class='divTableBody'>"; ?>
+		
+		<?php echo "<div class='divTableRow'>"; ?>
+		
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo the_terms( $post->ID, "genus" ); ?>
+		<?php echo "</div>"; ?>
+		
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo the_terms( $post->ID, "species" ); ?>
+		<?php echo "</div>"; ?>
+		
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo the_terms( $post->ID, "common_names" ); ?>
+		<?php echo "</div>"; ?>
+		<?php echo "</div>"; ?>		
+		<?php echo "</div></div>"; ?>
+		
+		<?php echo "</br>"; ?>
+		<div class="ref-field">
+		<div class="ref-label ref-published-label">Comments:</div>
+		<?php the_field('comments'); ?>	
+		<?php echo "</div>"; ?>
+
+		<?php echo "<div class='divTable plants-table'>"; ?>		
+		<?php echo "<div class='divTableBody'>"; ?>
+		
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Location</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo the_terms( $post->ID, "locations" ); ?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+		
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Bloom Times</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo labs_get_acf_string_value( 'bloom_times' ); ?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Origin</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo labs_get_acf_string_value( 'origin' ); ?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+		
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Interest/characteristic</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo labs_get_acf_string_value( 'characteristics' ); ?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Action Timeframe</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo labs_get_acf_string_value( 'action_timeframe' ); ?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Reference</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo the_field( 'reference' ); ?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Cross Reference</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php echo labs_get_acf_string_value( 'cross_reference' ); ?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Entry Date</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php 
+			$date = get_field('entry_dates', false, false);
+			$date = new DateTime($date);
+			echo $date->format('m/d/Y');
+			//echo the_field( 'entry_dates' ); 
+		?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+
+		<?php echo "<div class='divTableRow'>"; ?>
+		<?php echo "<div class='divTableCell'>Removal Date</div>"; ?>
+		<?php echo "<div class='divTableCell'>"; ?>
+		<?php 
+			$date = get_field('removal_dates', false, false);
+			$date = new DateTime($date);
+			echo $date->format('m/d/Y');
+			//echo the_field( 'removal_dates' ); 
+		?>
+		<?php echo "</div>"; ?> 
+		<?php echo "</div>"; ?>
+
+				
+		<?php echo "</div></div>"; ?>
+
 	<?php endif; ?>
 	
 	
